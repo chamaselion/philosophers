240bcc858015d8ae6e845c9f67db37d7f407b785
bszikora <chamaselion@gmail.com>
2024-10-31 12:29:55 +0100

    Added meal mutex to philos struct

--- philo.h
+++ philo.h
@@ -6,16 +6,19 @@
 /*   By: bszikora <bszikora@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 12:59:16 by bszikora          #+#    #+#             */
-/*   Updated: 2024/10/08 14:39:46 by bszikora         ###   ########.fr       */
+/*   Updated: 2024/10/31 12:17:52 by bszikora         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 # ifndef PHILO
 # define PHILO
 
-#include <stdlib.h>
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include "philo.h"
+#include <sys/time.h>
 
 typedef struct s_philoargs {
     int no_philosophers;
@@ -23,8 +26,18 @@ typedef struct s_philoargs {
     int time_to_eat;
     int time_to_sleep;
     int no_t_philosopher_must_eat;
-	int extra;
+    int extra;
+    pthread_mutex_t print_mutex;
+    pthread_mutex_t *forks;
 } t_philoargs;
+
+typedef struct s_philosopher {
+    int id;
+    pthread_mutex_t *fork;
+    t_philoargs *args;
+    long last_meal_time;
+    pthread_mutex_t meal_mutex; // Add this mutex
+} t_philosopher;
 
 // philo utilities
 int str_isdigit(char *i);
