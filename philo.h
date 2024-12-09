ba587049059edae61bcab5d52f94c371ef55a112
Balint Szikora <bszikora@student.42heilbronn.de>
2024-12-09 16:20:07 +0100

    Added functions to header to split into multiple files

--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: bszikora <bszikora@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 12:59:16 by bszikora          #+#    #+#             */
-/*   Updated: 2024/11/21 13:45:54 by bszikora         ###   ########.fr       */
+/*   Updated: 2024/12/09 16:18:16 by bszikora         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -39,12 +39,25 @@ typedef struct s_philosopher {
     t_philoargs *args;
     long last_meal_time;
 	int	times_eaten;
+	int	is_eating;
     pthread_mutex_t meal_mutex;
 } t_philosopher;
 
 // philo utilities
 int str_isdigit(char *i);
 int ft_atoi(const char *t);
+long get_time_of_day();
+void initialize_forks(t_philosopher *philo, pthread_mutex_t **left_fork, pthread_mutex_t **right_fork);
+
+//philo routines
+void *philo_routine(void *arg);
+void *monitor_routine(void *arg);
+
+//philo routine utis
+void precise_sleep(long duration);
+void print_state(t_philosopher *philo, const char *state, t_philoargs *args);
+void extra_checker(int *all_right, t_philosopher *philos, t_philoargs *args);
+
 
 // philo input handle
 int user_input_parse(t_philoargs *args, int argc, char **argv);
