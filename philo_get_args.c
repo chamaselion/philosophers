95ae6d4a1f059b5c7160a993c80755e3e1ae42ac
Balint Szikora <bszikora@2-H-6.42heilbronn.de>
2024-12-11 15:02:54 +0100

    Norminette stuff

--- philo_get_args.c
+++ philo_get_args.c
@@ -1,4 +1,4 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   philo_get_args.c                                   :+:      :+:    :+:   */
@@ -6,33 +6,37 @@
 /*   By: bszikora <bszikora@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 14:36:59 by bszikora          #+#    #+#             */
-/*   Updated: 2024/10/08 14:43:37 by bszikora         ###   ########.fr       */
+/*   Updated: 2024/12/11 14:18:40 by bszikora         ###   ########.fr       */
 /*                                                                            */
-/******************************************************************************/
+/* ************************************************************************** */
 
 #include "philo.h"
 
-int pre_check(int argc, char **argv, int *extra)
+int	pre_check(int argc, char **argv, int *extra)
 {
-	int c;
+	int	c;
 
 	c = 1;
 	if (argc == 5 || argc == 6)
 	{
-		*extra = (argc == 6) ? 1 : 0;
+		*extra = 0;
+		if (argc == 6)
+		{
+			*extra = 1;
+		}
 		while (c < argc)
 		{
 			if (str_isdigit(argv[c]) == 0)
-				return (-2); // -2 for non-integer args
+				return (-2);
 			c++;
 		}
 		return (0);
 	}
 	else
-		return (-1); // -1 for invalid number of args
+		return (-1);
 }
 
-void get_args(t_philoargs *args, char **argv)
+void	get_args(t_philoargs *args, char **argv)
 {
 	args->no_philosophers = ft_atoi(argv[1]);
 	args->time_to_die = ft_atoi(argv[2]);
@@ -42,9 +46,9 @@ void get_args(t_philoargs *args, char **argv)
 		args->no_t_philosopher_must_eat = ft_atoi(argv[5]);
 }
 
-int user_input_parse(t_philoargs *args, int argc, char **argv)
+int	user_input_parse(t_philoargs *args, int argc, char **argv)
 {
-	int err;
+	int	err;
 
 	err = pre_check(argc, argv, &args->extra);
 	if (err == -1)
