c6bb96b341093ae16e712b48fb6945e117eb83d2
Balint Szikora <bszikora@2-H-6.42heilbronn.de>
2024-12-11 15:03:13 +0100

    Norminette stuff

--- philo_main.c
+++ philo_main.c
@@ -1,4 +1,4 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   philo_main.c                                       :+:      :+:    :+:   */
@@ -6,90 +6,96 @@
 /*   By: bszikora <bszikora@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 12:59:05 by bszikora          #+#    #+#             */
-/*   Updated: 2024/12/09 17:04:49 by bszikora         ###   ########.fr       */
+/*   Updated: 2024/12/11 15:00:17 by bszikora         ###   ########.fr       */
 /*                                                                            */
-/******************************************************************************/
+/* ************************************************************************** */
 
 #include "philo.h"
 
-void threads_join(t_philoargs *args, pthread_t *thread)
+void	threads_join(t_philoargs *args, pthread_t *thread)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (i < args->no_philosophers)
-    {
-        pthread_join(thread[i], NULL);
-        i = i + 1;
-    }
-}
-
-void threads_free(t_philoargs *args, pthread_t *thread, t_philosopher *philo)
-{
-    int i;
-
-    i = 0;
-    while (i < args->no_philosophers)
-    {
-        pthread_mutex_destroy(&args->forks[i]);
-        pthread_mutex_destroy(&philo[i].meal_mutex);
-        i++;
-    }
-    pthread_mutex_destroy(&args->print_mutex);
-    pthread_mutex_destroy(&args->terminate_mutex);  // Add this line
-    free(args->forks);
-    free(thread);
-    free(philo);
-}
-
-void threads_init(t_philoargs *args, pthread_t **thread, t_philosopher **philo)
-{
-    int i;
-
-    i = 0;
-    *thread = malloc(sizeof(pthread_t) * args->no_philosophers);
-    *philo = malloc(sizeof(t_philosopher) * args->no_philosophers);
-    args->forks = malloc(sizeof(pthread_mutex_t) * args->no_philosophers);
-    pthread_mutex_init(&args->print_mutex, NULL);
-	pthread_mutex_init(&args->terminate_mutex, NULL);  // Add this line
-    args->should_terminate = 0;
-    while (i < args->no_philosophers)
-    {
-        pthread_mutex_init(&args->forks[i], NULL);
-        pthread_mutex_init(&(*philo)[i].meal_mutex, NULL);
-        (*philo)[i].id = i + 1;
-        (*philo)[i].fork = &args->forks[i];
-        (*philo)[i].args = args;
-        (*philo)[i].last_meal_time = get_time_of_day();
-		(*philo)[i].times_eaten = 0;
-        i++;
-    }
-    i = 0;
-    while (i < args->no_philosophers)
-    {
-        pthread_create(&(*thread)[i], NULL, philo_routine, &(*philo)[i]);
-        i++;
-    }
-}
-
-int main(int argc, char **argv)
-{
-    t_philoargs args;
-    pthread_t *thread;
-    t_philosopher *philo;
-    int parse_error;
-    pthread_t monitor_thread;
-    thread = NULL;
-    philo = NULL;
-    parse_error = user_input_parse(&args, argc, argv);
-    if (parse_error)
+	i = 0;
+	while (i < args->no_philosophers)
 	{
-        return (1);
+		pthread_join(thread[i], NULL);
+		i = i + 1;
+	}
+}
+
+void	threads_free(t_philoargs *args, pthread_t *thread, t_philosopher *philo)
+{
+	int	i;
+
+	i = 0;
+	while (i < args->no_philosophers)
+	{
+		pthread_mutex_destroy(&args->forks[i]);
+		pthread_mutex_destroy(&philo[i].meal_mutex);
+		i++;
+	}
+	pthread_mutex_destroy(&args->print_mutex);
+	pthread_mutex_destroy(&args->terminate_mutex);
+	free(args->forks);
+	free(thread);
+	free(philo);
+}
+
+void	initialize_philosopher(t_philosopher *philo, t_philoargs *ar, int i)
+{
+	pthread_mutex_init(&ar->forks[i], NULL);
+	pthread_mutex_init(&philo[i].meal_mutex, NULL);
+	philo[i].id = i + 1;
+	philo[i].fork = &ar->forks[i];
+	philo[i].args = ar;
+	philo[i].last_meal_time = get_time_of_day();
+	philo[i].times_eaten = 0;
+}
+
+void	threads_init(t_philoargs *ar, pthread_t **thread, t_philosopher **philo)
+{
+	int	i;
+
+	i = 0;
+	*thread = malloc(sizeof(pthread_t) * ar->no_philosophers);
+	*philo = malloc(sizeof(t_philosopher) * ar->no_philosophers);
+	ar->forks = malloc(sizeof(pthread_mutex_t) * ar->no_philosophers);
+	pthread_mutex_init(&ar->print_mutex, NULL);
+	pthread_mutex_init(&ar->terminate_mutex, NULL);
+	ar->should_terminate = 0;
+	while (i < ar->no_philosophers)
+	{
+		initialize_philosopher(*philo, ar, i);
+		i++;
+	}
+	i = 0;
+	while (i < ar->no_philosophers)
+	{
+		pthread_create(&(*thread)[i], NULL, philo_routine, &(*philo)[i]);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	t_philoargs		args;
+	pthread_t		*thread;
+	t_philosopher	*philo;
+	int				parse_error;
+	pthread_t		monitor_thread;
+
+	thread = NULL;
+	philo = NULL;
+	parse_error = user_input_parse(&args, argc, argv);
+	if (parse_error)
+	{
+		return (1);
 	}
 	args.firstime = get_time_of_day();
-    threads_init(&args, &thread, &philo);
-    pthread_create(&monitor_thread, NULL, monitor_routine, philo);
-    threads_join(&args, thread);
-    threads_free(&args, thread, philo);
-    return (0);
+	threads_init(&args, &thread, &philo);
+	pthread_create(&monitor_thread, NULL, monitor_routine, philo);
+	threads_join(&args, thread);
+	threads_free(&args, thread, philo);
+	return (0);
 }
